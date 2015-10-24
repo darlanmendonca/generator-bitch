41dcebf429bd78cebf9c6791f0dfbf6fb834c037
darlanmendonca <darlanmendonca@gmail.com>
2015-10-24 21:29:52 -0200

    add NODE_ENV test to integration tests

--- app/templates/express/config/index.js
+++ app/templates/express/config/index.js
@@ -26,10 +26,23 @@ let config = {
     token: {
       expiresInMinutes: 1440 // 24 hours
     }
+  },
+  test: {
+    server: {
+      port: 3005,
+      proxy: 5005
+    },
+    database: {
+      url: 'mongodb://localhost/<%= slugify(appname) %>_test'
+    },
+    secret: '<%= appSecret %>',
+    token: {
+      expiresInMinutes: 1440 // 24 hours
+    }
   }
 };
 
 let shell = require('shell-arguments');
-let env = shell.env || process.env.ENV || 'production';
+let env = shell.env || process.env.NODE_ENV || 'production';
 
 module.exports = config[env];
